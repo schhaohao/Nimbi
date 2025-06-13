b1c589f6a3b915d45ca2738629502fb8e4842fb7
schhaohao <sunchenhao518@163.com>
2025-06-13 20:58:48 +0800

    init the Niubi project

---
+++ src/main/java/com/niubi/niubi/NiubiApplication.java
@@ -0,0 +1,13 @@
+package com.niubi.niubi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NiubiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NiubiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/niubi/niubi/NiubiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.niubi.niubi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NiubiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
